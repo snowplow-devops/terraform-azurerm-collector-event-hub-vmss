2104f5a6347848ee94d5332cf1cc4dde65dd4414
jbeemster <joshua.a.beemster@gmail.com>
2023-10-02 22:17:58 +0800

    Prepare for release

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   module_name    = "collector-event-hub-vmss"
-  module_version = "0.1.1"
+  module_version = "0.2.0"
 
   app_name    = "stream-collector"
   app_version = var.app_version
